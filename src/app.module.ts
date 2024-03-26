0567f38bf4de8ac904cbc1a1d086ee2880d57ca8
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-03-27 01:31:50 +0500

    feat: add modules to app, enable cors & helmet & update server start command

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,50 @@
-import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from '../config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MediaModule } from './media/media.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './common/guards/auth.guard';
+import { RolesGuard } from './common/guards/roles.guard';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('database.url'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+      ignoreEnvFile: false,
+    }),
+    MediaModule,
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.use(helmet());
+  app.enableCors();
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<string>('port'));
 }
 bootstrap();
