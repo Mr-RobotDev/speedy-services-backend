357de7b89449c88b4b45a243689f336ea40157d5
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-06-12 00:59:48 +0500

    remove: unnecessary webhook resource files

--- src/webhook/schema/webhook.schema.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
-
-@Schema({
-  timestamps: true,
-})
-export class Webhook extends Document {}
-
-export const WebhookSchema = SchemaFactory.createForClass(Webhook);
